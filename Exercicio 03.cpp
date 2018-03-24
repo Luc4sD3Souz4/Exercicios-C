fe288ac089e88bd1c1e9f9527ff8ea00a838aaef
TheFenix007 <37718781+TheFenix007@users.noreply.github.com>
2018-03-23 21:49:29 -0300

    Rename Exercicio 3.cpp to Exercicio 03.cpp

--- Exercicio 03.cpp
+++ Exercicio 03.cpp
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-Descobre se seu número
-é par ou ímpar
+Descobre se seu nÃºmero
+Ã© par ou Ã­mpar
 */
 int main() {
-	// float é o tipo da variável que vai armazenar um número com casas decimais
-	// int = número inteiro
-	// float = número com casas decimais
+	// float Ã© o tipo da variÃ¡vel que vai armazenar um nÃºmero com casas decimais
+	// int = nÃºmero inteiro
+	// float = nÃºmero com casas decimais
 	int   resto,
 		  numero;
-		// valor das variáveis
+		// valor das variÃ¡veis
 		resto = 0;
 		numero = 0;
-		// impressão das linhas na tela
+		// impressÃ£o das linhas na tela
 		printf("********************\n");
 		printf("Digite um numero\n");
 		// scanf = digite algo
-		// %f = é um float
-		// %d = é um int
-		// &variável = qual variável usar
+		// %f = Ã© um float
+		// %d = Ã© um int
+		// &variÃ¡vel = qual variÃ¡vel usar
 		scanf("%d",&numero);
 		printf("********************\n");
 		resto = numero % 2; // % significa resto
@@ -28,6 +28,6 @@ int main() {
 	    }else{
 	    	printf("Seu numero e impar\n\n");	
 	    }
-		system("pause"); // não permite que a janela do prompt feche
+		system("pause"); // nÃ£o permite que a janela do prompt feche
 		return 0;
 }
